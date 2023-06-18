87b46cd5d2bba50a03ef1e25c645cce89e324dff
tech-teni <yakubganiyat96@gmail.com>
2023-06-19 00:56:58 +0100

    all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { OldSocialLogin as SocialLogin } from "react-social-login";
-
-
+import { logout } from "react-social-login";
 
 function App() {
   const handleSocialLogin = (user, err) => {
     console.log(user);
     console.log(err);
   };
-  
+
   return (
     <div className="App">
       <h1>hello</h1>
       <SocialLogin
-      provider="facebook"
-      appId="821888192448241"
-      callback={handleSocialLogin}
-    >
-      <button>Login with Google</button>
-    </SocialLogin>
+        provider="facebook"
+        appId="821888192448241"
+        callback={handleSocialLogin}
+        onLoginFailure={(err) => {
+          console.log(err);
+        }}
+      >
+        <button>Login with facebook</button>
+      </SocialLogin>
+
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          logout();
+        }}
+      >
+        Log out
+      </button>
     </div>
   );
 }
