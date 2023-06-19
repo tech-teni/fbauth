72eb4d67c5bc65cd5f04bb2ac40a2d9016691bcc
tech-teni <yakubganiyat96@gmail.com>
2023-06-19 14:06:44 +0100

    all

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { OldSocialLogin as SocialLogin } from "react-social-login";
-import { logout } from "react-social-login";
+import FacebookLogin from 'react-facebook-login';
 
 function App() {
   const handleSocialLogin = (user, err) => {
@@ -9,38 +10,44 @@ function App() {
     console.log(err);
   };
 
+ const  responseFacebook=(response)=> {
+    console.log(response);
+  }
+
   return (
     <div className="App">
       <h1>hello</h1>
-      <SocialLogin
-        provider="facebook"
-        appId="821888192448241"
-        callback={handleSocialLogin}
-        onLoginFailure={(err) => {
-          console.log(err);
-        }}
-      >
-        <button>Login with facebook</button>
-      </SocialLogin>
+      <button>
+        <SocialLogin
+          onClick={(e) => {
+            console.log("hejej");
+          }}
 
+          provider="facebook"
+          appId="821888192448241"
+          callback={handleSocialLogin}
+          onLoginFailure={(err) => {
+            console.log(err);
+          }}
+        >
+          Login with facebook
+        </SocialLogin>
+      </button>
       <button
+      
         onClick={(e) => {
           e.preventDefault();
-          
         }}
-      >      
-      <SocialLogin
-      provider="facebook"
-      appId="821888192448241"
-      callback={handleSocialLogin}
-      onLogoutSuccess={(err) => {
-        console.log(err);
-      }}
-    >
-              Log out
-
-    </SocialLogin>
-
+      >
+         <FacebookLogin
+          appId="1088597931155576"
+          autoLoad={true}
+          fields="name,email,picture"
+          scope="public_profile,user_friends,user_actions.books"
+          callback={responseFacebook}
+        >
+        Log in with facebook react
+        </FacebookLogin>
       </button>
     </div>
   );
